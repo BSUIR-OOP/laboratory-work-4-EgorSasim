9163b6b5f40938fec77df1c7de1ff1350adc89fd
Gorgy <egorsasim@mail.ru>
2022-05-30 13:05:29 +0300

    add little app example

---
+++ lb4/IMessageWriter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lb4
+{
+    internal interface IMessageWriter
+    {
+        public string Id { get; }
+        public void Write();
+    }
+}
